9464c60add0a56fb8283b92ee63e0e70e99b408e
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-05-15 23:18:54 +0900

    add root widget

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class EditorScreen(Screen):
+    pass
+
+
+class SprateScreenManager(ScreenManager):
+    pass
 
 
 class SprateApp(App):
-    
+    def build(self):
+        root_widget = SprateScreenManager()
+        root_widget.add_widget(EditorScreen(name="editor"))
+        return root_widget
+
 
 if __name__ == "__main__":
     pass
